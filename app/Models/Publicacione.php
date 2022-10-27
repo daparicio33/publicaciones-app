8e7d4f522a7bcc6df652acfae6b13803df050363
dave33 <dwaparicicio@gmail.com>
2022-10-27 11:56:11 -0500

    creacion de migraciones y modelos con su relaciones

---
+++ app/Models/Publicacione.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Publicacione extends Model
+{
+    use HasFactory;
+    public function usuario(){
+        return $this->belongsTo(Usuario::class);
+    }
+    public function comentarios(){
+        return $this->hasMany(Comentario::class);
+    }
+}
