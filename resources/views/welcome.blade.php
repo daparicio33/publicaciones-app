14d0c497909e6c3b38d048675f9d34794aacc678
daparicio55 <daparicio@idexperujapon.edu.pe>
2022-10-27 14:51:44 -0500

    vista principal del sistema

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,64 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Publicaciones')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Publicaciones del Sistema</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Aca ponemos las publicaciones del sistema.</p>
+    {{-- inicio de la publicacion --}}
+    <div class="card mb-3" style="max-width: 100%;">
+        <div class="row g-0">
+          <div class="col-md-3">
+            <img src="{{ asset('vendor/adminlte/dist/img/AdminLTELogo.png') }}" class="img-fluid rounded mx-auto d-block" alt="...">
+          </div>
+          <div class="col-md-9">
+            <div class="card-header">
+                <h5 class="card-title">Publicacion 1</h5>
+            </div>
+            <div class="card-body">
+              
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Nombre de Autor</small></p>
+                <h6>Comentarios</h6>
+                <div class="card">
+                    <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-muted">05-08-2022</h6>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-muted">05-08-2022</h6>
+                    <p class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                    </div>
+                </div>
+                <h6>Nuevo Comentario</h6>
+                <div class="card">
+                    <div class="card-body">
+                        <textarea name="" id="" rows="5" class="form-control"></textarea>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fas fa-file-export"></i> Enviar
+                        </button>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    {{-- fin de publicacion --}}
 @stop
 
 @section('css')
