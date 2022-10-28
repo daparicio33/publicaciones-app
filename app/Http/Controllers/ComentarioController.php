176f32314c9dee9b8faf91ece9dc6656d540ed2f
DanteAC01 <carlos.oxidentexd@gmail.com>
2022-10-28 11:39:35 -0500

    correciones ComentarioController

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comentario;
+use App\Models\Publicacione;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use PhpParser\Node\Stmt\Return_;
@@ -12,35 +14,35 @@ class ComentarioController extends Controller
     //
     public function index(){
         $comentarios=Comentario::all();
-        return view('comentario.index', compact('clientes'));
+        return view('comentarios.index', compact('comentarios'));
     }
 
     public function create(){
-        return view('comentario.create');
+        $publicaciones=Publicacione::all();
+        return view('comentarios.create', compact('publicaciones'));
     }
 
     public function store(Request $request){
         $comentario = new Comentario();
         $comentario->descripcion=$request->descripcion;
+        $comentario->publicacione_id=$request->publicacione_id;
         $comentario->save();
-        Return Redirect::to('comentario');
-        $comentario->save();
-        Return Redirect::to('comentario');
+        return Redirect::to('comentarios');
     }
 
     public function edit($id){
         $comentario = Comentario::findOrFail($id);
-        return view('comentario.edit', compact('comenrario'));
+        return view('comentarios.edit', compact('comentarios'));
         }
     public function update($id, Request $request){
         $comentario = Comentario::finOrFail($id);
         $comentario->descripcion=$request->descripcion;
         $comentario->update();
-        Return Redirect::to('comentario');
+        return Redirect::to('comentarios');
     }
     public function delete($id){
         $comentario = Comentario::finOrFail($id);
         $comentario->delete();
-        return Redirect::to('comentario');
+        return Redirect::to('comentarios');
     }
 }
