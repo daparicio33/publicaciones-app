6a606c0b97d9f8717199efe48fafe7c5af338616
dave33 <dwaparicicio@gmail.com>
2022-10-28 12:09:33 -0500

    create modigicado

--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -6,18 +6,18 @@
 @section('content')
 <form action="{{ asset('usuarios') }}" method="post">
     @csrf
-    <label for="nombre">nombre</label>
-    <input type="text" name="nombre">
-
-    <label for="apellido">apellido</label>
-    <input type="text" name="apellido">
-
-    <label for="dni">DNI</label>
-    <input type="text" name="dni">
-
-    <label for="foto">foto</label>
-    <input type="text" name="foto">
-
-    <button type="submit">Enviar</button>
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+            <div class="form-group">
+                <label for="nombre">nombre</label>
+                <input class="form-control" type="text" name="nombre">
+                <label for="apellido">apellido</label>
+                <input class="form-control" type="text" name="email">
+                <label for="dni">DNI</label>
+                <input class="form-control" type="text" name="dni">
+                <button class="btn btn-summit" type="submit">Enviar</button>
+            </div>
+        </div>
+    </div>
 </form>
 @endsection
