30b0e84941551961c80c179ce87bc31b707e4f99
kevin <yoplackevin@gmail.com>
2022-10-28 10:12:20 -0500

    Vista principal de la tabla publicaciones hecha

--- database/migrations/2022_10_27_164456_create_usuarios_table.php
+++ database/migrations/2022_10_27_164456_create_usuarios_table.php
@@ -14,7 +14,7 @@ class CreateUsuariosTable extends Migration
     public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
+            
             $table->string('nombre');
             $table->string('email');
             $table->string('dni')->unique();
